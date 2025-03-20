8410d621b28447fa8fec5bcb11b5fa317f87c5da
Phạm Văn Chiến <phamvanchien3605@gmail.com>
2025-03-20 22:16:31 +0700

    remove setting attribute

--- views/project/setting/ProjectSettingView.tsx
+++ views/project/setting/ProjectSettingView.tsx
@@ -13,7 +13,6 @@ import { notify } from "@/utils/helper.util";
 import { catchError } from "@/services/base.service";
 import ErrorAlert from "@/common/components/ErrorAlert";
 import { useTranslations } from "next-intl";
-import ProjectSettingAttribute from "./components/ProjectSettingAttribute";
 
 interface ProjectSettingViewProps {
   project: ProjectType
